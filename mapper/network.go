d42dcecfddfdf6c464dcc6a4f57d2d072c894422
Tom Bevan <tom.bevan@r3labs.io>
2016-10-25 12:44:14 +0100

    fixing network mappings

--- mapper/network.go
+++ mapper/network.go
@@ -5,6 +5,12 @@
 package mapper
 
 import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
 	"github.com/ernestio/vcloud-definition-mapper/definition"
 	"github.com/ernestio/vcloud-definition-mapper/output"
 )
@@ -16,10 +22,17 @@ func MapNetworks(d definition.Definition) []output.Network {
 
 	for _, r := range d.Routers {
 		if d.IsSaltBootstrapped() {
+			subnet := "10.254.254.0/24"
+			octets := getIPOctets(subnet)
+
 			sn := output.Network{
 				Name:               d.GeneratedName() + "salt",
 				Type:               "$(datacenters.items.0.type)",
-				Subnet:             "10.254.254.0/24",
+				Subnet:             subnet,
+				StartAddress:       octets + ".5",
+				EndAddress:         octets + ".250",
+				Gateway:            octets + ".1",
+				Netmask:            parseNetmask(subnet),
 				RouterName:         "$(routers.items.0.name)",
 				RouterType:         "$(routers.items.0.type)",
 				RouterIP:           "$(routers.items.0.ip)",
@@ -36,10 +49,15 @@ func MapNetworks(d definition.Definition) []output.Network {
 		}
 
 		for _, network := range r.Networks {
+			octets := getIPOctets(network.Subnet)
 
 			n := output.Network{
 				Name:               d.GeneratedName() + network.Name,
 				Subnet:             network.Subnet,
+				StartAddress:       octets + ".5",
+				EndAddress:         octets + ".250",
+				Gateway:            octets + ".1",
+				Netmask:            parseNetmask(network.Subnet),
 				DNS:                network.DNS,
 				RouterName:         "$(routers.items.0.name)",
 				RouterType:         "$(routers.items.0.type)",
@@ -58,4 +76,24 @@ func MapNetworks(d definition.Definition) []output.Network {
 	}
 
 	return networks
+}
+
+func getIPOctets(rng string) string {
+	// Splits the network range and returns the first three octets
+	ip, _, err := net.ParseCIDR(rng)
+	if err != nil {
+		log.Println(err)
+	}
+	octets := strings.Split(ip.String(), ".")
+	octets = append(octets[:3], octets[3+1:]...)
+	octetString := strings.Join(octets, ".")
+	return octetString
+}
+
+func parseNetmask(rng string) string {
+	// Convert netmask hex to string, generated from network range CIDR
+	_, nw, _ := net.ParseCIDR(rng)
+	hx, _ := hex.DecodeString(nw.Mask.String())
+	netmask := fmt.Sprintf("%v.%v.%v.%v", hx[0], hx[1], hx[2], hx[3])
+	return netmask
 }
